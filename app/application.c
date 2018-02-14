b4e8ab27ab2363eedcef68f2244ea5883b644695
Karel Blavka <karel.blavka@bigclown.com>
2018-02-14 11:41:45 +0100

    Change measure interval
    First hour is service mode, then normal mode
    flood detector: 1s / 5s
    tempereture:  5s / 10s

--- app/application.c
+++ app/application.c
@@ -1,12 +1,15 @@
 #include <application.h>
 
+#define SERVICE_INTERVAL_INTERVAL (60 * 60 * 1000)
 #define BATTERY_UPDATE_INTERVAL (60 * 60 * 1000)
 #define TEMPERATURE_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
 #define TEMPERATURE_PUB_VALUE_CHANGE 0.2f
-#define TEMPERATURE_UPDATE_INTERVAL (1 * 1000)
+#define TEMPERATURE_UPDATE_SERVICE_INTERVAL (5 * 1000)
+#define TEMPERATURE_UPDATE_NORMAL_INTERVAL (10 * 1000)
 
 #define FLOOD_DETECTOR_NO_CHANGE_INTEVAL (15 * 60 * 1000)
-#define FLOOD_DETECTOR_UPDATE_INTERVAL (1 * 1000)
+#define FLOOD_DETECTOR_UPDATE_SERVICE_INTERVAL (1 * 1000)
+#define FLOOD_DETECTOR_UPDATE_NORMAL_INTERVAL  (5 * 1000)
 
 #define RADIO_FLOOD_DETECTOR        0x0d
 
@@ -16,8 +19,8 @@ bc_led_t led;
 // Button instance
 bc_button_t button;
 
-// Temperature instance
-bc_tag_temperature_t temperature;
+// Thermometer instance
+bc_tmp112_t tmp112;
 static event_param_t temperature_event_param = { .next_pub = 0 };
 
 // Flood detector instance
@@ -48,16 +51,16 @@ void battery_event_handler(bc_module_battery_event_t event, void *event_param)
     }
 }
 
-void temperature_tag_event_handler(bc_tag_temperature_t *self, bc_tag_temperature_event_t event, void *event_param)
+void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *event_param)
 {
     float value;
     event_param_t *param = (event_param_t *)event_param;
 
-    if (event == BC_TAG_TEMPERATURE_EVENT_UPDATE)
+    if (event == BC_TMP112_EVENT_UPDATE)
     {
-        if (bc_tag_temperature_get_temperature_celsius(self, &value))
+        if (bc_tmp112_get_temperature_celsius(self, &value))
         {
-            if ((fabs(value - param->value) >= TEMPERATURE_PUB_VALUE_CHANGE) || (param->next_pub < bc_scheduler_get_spin_tick()))
+            if ((fabsf(value - param->value) >= TEMPERATURE_PUB_VALUE_CHANGE) || (param->next_pub < bc_scheduler_get_spin_tick()))
             {
                 bc_radio_pub_temperature(param->channel, &value);
 
@@ -88,6 +91,15 @@ void flood_detector_event_handler(bc_flood_detector_t *self, bc_flood_detector_e
     }
 }
 
+void switch_to_normal_mode_task(void *param)
+{
+    bc_tmp112_set_update_interval(&tmp112, TEMPERATURE_UPDATE_NORMAL_INTERVAL);
+
+    bc_flood_detector_set_update_interval(&flood_detector, FLOOD_DETECTOR_UPDATE_NORMAL_INTERVAL);
+
+    bc_scheduler_unregister(bc_scheduler_get_current_task_id());
+}
+
 void application_init(void)
 {
     // Initialize LED
@@ -106,18 +118,20 @@ void application_init(void)
     bc_module_battery_set_event_handler(battery_event_handler, NULL);
     bc_module_battery_set_update_interval(BATTERY_UPDATE_INTERVAL);
 
-    // Initialize temperature
+    // Initialize thermometer sensor on core module
     temperature_event_param.channel = BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_ALTERNATE;
-    bc_tag_temperature_init(&temperature, BC_I2C_I2C0, BC_TAG_TEMPERATURE_I2C_ADDRESS_ALTERNATE);
-    bc_tag_temperature_set_update_interval(&temperature, TEMPERATURE_UPDATE_INTERVAL);
-    bc_tag_temperature_set_event_handler(&temperature, temperature_tag_event_handler, &temperature_event_param);
+    bc_tmp112_init(&tmp112, BC_I2C_I2C0, 0x49);
+    bc_tmp112_set_event_handler(&tmp112, tmp112_event_handler, &temperature_event_param);
+    bc_tmp112_set_update_interval(&tmp112, TEMPERATURE_UPDATE_SERVICE_INTERVAL);
 
     // Initialize flood detector
     bc_flood_detector_init(&flood_detector, BC_FLOOD_DETECTOR_TYPE_LD_81_SENSOR_MODULE_CHANNEL_A);
     bc_flood_detector_set_event_handler(&flood_detector, flood_detector_event_handler, &flood_detector_event_param);
-    bc_flood_detector_set_update_interval(&flood_detector, FLOOD_DETECTOR_UPDATE_INTERVAL);
+    bc_flood_detector_set_update_interval(&flood_detector, FLOOD_DETECTOR_UPDATE_SERVICE_INTERVAL);
 
     bc_radio_pairing_request("kit-flood-detector", VERSION);
+
+    bc_scheduler_register(switch_to_normal_mode_task, NULL, SERVICE_INTERVAL_INTERVAL);
 
     bc_led_pulse(&led, 2000);
 }
